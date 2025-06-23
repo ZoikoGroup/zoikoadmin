fc3b6d710c5ce4f774a13f70c8a55615126a1677
Anowar Sadat <anowar.vista@gmail.com>
2025-06-23 16:36:19 +0530

    Admin and menu Item updates

--- app/Filament/Resources/MenuItemResource/Pages/EditMenuItem.php
+++ app/Filament/Resources/MenuItemResource/Pages/EditMenuItem.php
@@ -16,4 +16,9 @@ class EditMenuItem extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index'); 
+    }
 }
